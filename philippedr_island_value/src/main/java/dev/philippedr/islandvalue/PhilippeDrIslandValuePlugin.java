c389d0c4b3208f2bbe9f9b81d9309120406e5725
BuildTools <cleaver9078@protonmail.com>
2025-12-29 04:32:18 +0100

    Add bStats integration for metrics tracking and update POM dependencies

--- philippedr_island_value/src/main/java/dev/philippedr/islandvalue/PhilippeDrIslandValuePlugin.java
+++ philippedr_island_value/src/main/java/dev/philippedr/islandvalue/PhilippeDrIslandValuePlugin.java
@@ -4,16 +4,28 @@ import dev.philippedr.islandvalue.commands.IsValueCommand;
 import dev.philippedr.islandvalue.hologram.HolographicValue;
 import dev.philippedr.islandvalue.superiorskyblock.SuperiorSkyblock2Connector;
 import dev.philippedr.islandvalue.superiorskyblock.SuperiorSkyblock2IslandValueCheck;
+import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PhilippeDrIslandValuePlugin extends JavaPlugin {
+
+    // https://bstats.org/what-is-my-plugin-id
+    private static final int BSTATS_PLUGIN_ID = 28576;
 
     private HolographicValue holographicValue;
 
     @Override
     public void onEnable() {
         SuperiorSkyblock2Connector connector = new SuperiorSkyblock2Connector();
+
+        Metrics metrics = new Metrics(this, BSTATS_PLUGIN_ID);
+        metrics.addCustomChart(new SimplePie(
+            "superiorskyblock2_present",
+            () -> connector.isAvailable() ? "yes" : "no"
+        ));
+
         SuperiorSkyblock2IslandValueCheck islandValueCheck = new SuperiorSkyblock2IslandValueCheck(connector);
         IsValueCommand isValueCommand = new IsValueCommand(this, islandValueCheck);
 
